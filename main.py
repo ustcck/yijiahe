704633138f4dd9a3d7b7c54d9eeb9b4fb53de02f
Cao Kai <caokai@yijiahe.com>
2018-01-08 20:03:44 +0800

    Fix[求和]:a+b
    [Description]:
    [Root Cause]:
    [Solution]:
    [Test Suggestion]:
    
    Signed-off-by: Cao Kai <caokai@yijiahe.com>

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 ﻿#!/usr/bin/python3
 #coding=utf-8
 
-print ("Life is short, you need Python !")
-
 a = 10
 b = 20
 
-c = a + b
+def sum(a, b):
+    return (a+b)
+
+c = sum(a, b)
 print("The value of c is  ", c)
